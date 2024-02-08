abb3c267846e84f44fbcd12c4e0f6f0fdb01a497
GustavoS11 <gustavohenrique6224@gmail.com>
2024-02-07 21:08:46 -0300

    alteração na rota do usuario

--- routes/user.rota.js
+++ routes/user.rota.js
@@ -1,8 +1,7 @@
 import {Router} from 'express'
-import {usuarios, cadastro, login, loginAdmin} from '../controllers/user.controller.js'
+import {usuarios, cadastro, login} from '../controllers/user.controller.js'
 export const rotaUsers = Router()
 
 rotaUsers.get('',usuarios)
 rotaUsers.post('/register',cadastro)
 rotaUsers.post('/login',login)
-rotaUsers.post('/loginAdmin', loginAdmin)
